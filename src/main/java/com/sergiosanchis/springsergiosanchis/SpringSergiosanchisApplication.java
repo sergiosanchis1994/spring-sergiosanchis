3675f3789b37d285d79a02f313344daf127accfa
Sergio <sergiosanchis1994@gmail.com>
2019-02-06 20:18:50 +0100

    First commit

---
+++ src/main/java/com/sergiosanchis/springsergiosanchis/SpringSergiosanchisApplication.java
@@ -0,0 +1,14 @@
+package com.sergiosanchis.springsergiosanchis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSergiosanchisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSergiosanchisApplication.class, args);
+	}
+
+}
+
